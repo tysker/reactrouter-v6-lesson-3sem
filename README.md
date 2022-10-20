c243b9428db6de2d22c80f19ca4933140ed8bcc4
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-20 16:40:33 +0200

    setup for main branch

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Router Version 6
+
+### Description
+
+1. npm install
+2. npm run dev
+
+### Hints
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h2>React Router Version 6</h2>
   </React.StrictMode>
 )
