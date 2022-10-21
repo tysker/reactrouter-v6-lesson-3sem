ede0db8736216920ff41a972071f0766121528f1
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-21 12:17:59 +0200

    lesson-06

--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -1,5 +1,5 @@
 import {getWorkshops} from "../api";
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 export default function Workshops() {
     const workshops = getWorkshops();
@@ -8,9 +8,11 @@ export default function Workshops() {
             <h1>Workshop Categories</h1>
             <ul className="workshops">
                 {workshops.map(work => (
-                    <li key={work.id}>
-                        {work.name}
-                    </li>
+                    <Link to={work.id}>
+                        <li key={work.id}>
+                            {work.name}
+                        </li>
+                    </Link>
                 ))}
             </ul>
             <Outlet/>
