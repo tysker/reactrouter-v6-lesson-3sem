4ae3cf85f2bfd3bb172f927bb57c26d9a6d28684
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-21 11:43:30 +0200

    lesson-06

--- src/components/Workshop.jsx
+++ src/components/Workshop.jsx
@@ -1,9 +1,27 @@
+import {Link, useParams} from "react-router-dom";
+import {getWorkshop} from "../api";
+
 export default function Workshop() {
+    const {workId} = useParams();
+    const workshop = getWorkshop(workId);
 
     return (
         <>
-            <h2>Workshop Sessions</h2>
-            <p>Workshop session content displays here...</p>
+            <h2>{workshop.name} Sessions</h2>
+
+            <ul className="session-list">
+                {workshop?.sessions.map(session => (
+                    <li className="session" key={session.id}>
+                        <Link
+                            to={session.id}>
+                            <p className="session-name">{session.name}</p>
+                            <p>
+                                {session.speaker.name} | {session.speaker.org}
+                            </p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
