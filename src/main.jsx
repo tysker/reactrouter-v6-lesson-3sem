8996e61854ddda3e53d60cedb82573aeab8fba8e
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-21 14:00:53 +0200

    lesson-08

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,21 @@ import Workshops from "./components/Workshops";
 import Header from "./components/Header";
 import About from "./components/About.jsx";
 import Workshop from "./components/Workshop";
+import Session from "./components/Session.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <div className="main">
                 <Header/>
-
                 <Routes>
-                    <Route path="/" element={<Home titel="Welcome to SchoolHacks"/>}/>
+                    <Route path="/" element={<Home title="Welcome to SchoolHacks"/>}/>
                     <Route path="about" element={<About/>}/>
                     <Route path="workshops" element={<Workshops/>}>
-                        <Route path=":workId" element={<Workshop/>}/>
+                        <Route path=":workId" element={<Workshop/>}>
+                            <Route path=":sessionId" element={<Session/>}/>
+                        </Route>
+                        <Route index element={<h3>Select a workshop from above</h3>}/>
                     </Route>
                     <Route path="*" element={<h1 className="not-found">Page Not Found</h1>}/>
                 </Routes>
